963b59a8a7d040732983c1af74f7eb61dc97c9e2
Feifan Wang <feifanw@gmail.com>
2017-01-13 11:36:10 -0500

    refactor the complex guard clauses into their own macros

--- lib/drom.ex
+++ lib/drom.ex
@@ -1,30 +1,62 @@
 defmodule Drom do
-  def find_palindrome(nil), do: nil
-  def find_palindrome(str) when is_bitstring(str) do
-    find_palindrome(str, 0, String.length(str) - 1)
-  end
+  @min_palindrome_len 3
 
-  defp find_palindrome(str, current_position, scan_len)
-    when (scan_len != byte_size(str) - 1) and ((current_position + scan_len) >= byte_size(str) - 1),
-    do: find_palindrome(str, 0, scan_len - 1)
-
-  defp find_palindrome(str, current_position, scan_len) do
-    end_position = current_position + scan_len
-    scanned_substr = String.slice(str, current_position..end_position)
-
-    case is_palindrome(scanned_substr) do
-      true -> {:ok, scanned_substr}
-      :too_short when scan_len > 2 ->
-        find_palindrome(str, 0, scan_len - 1)
-      :too_short -> :not_found
-      _ -> find_palindrome(str, current_position + 1, scan_len)
+  defmacro not_scanning_entire_str(str, scan_len) do
+    # scan_len != byte_size(str) - 1
+    quote do
+      unquote(scan_len) != byte_size(unquote(str)) - 1
     end
   end
 
+  defmacro substr_already_scanned(str, current_pos, scan_len) do
+    # ((current_pos + scan_len) >= byte_size(str) - 1),
+    quote do
+      ((unquote(current_pos) + unquote(scan_len)) >= byte_size(unquote(str)) - 1)
+    end
+  end
+
+  def find(nil), do: nil
+  def find(str) when is_bitstring(str) do
+    find(str, 0, String.length(str) - 1, [])
+  end
+
+  defp find(str, current_pos, scan_len, found)
+    when not_scanning_entire_str(str, scan_len)
+    and  substr_already_scanned(str, current_pos, scan_len),
+    do: find(str, 0, scan_len - 1, found)
+
+  defp find(str, current_pos, scan_len, found) do
+    scanned_substr = scan_substr(str, current_pos, scan_len)
+    last_found = last_found(found)
+
+    case is_palindrome(scanned_substr) do
+      {true, :ok} when byte_size(scanned_substr) < byte_size(last_found) ->
+        found
+      {true, :ok} ->
+        find(str, current_pos + 1, scan_len, [scanned_substr|found])
+      {false, :too_short} when scan_len > (@min_palindrome_len - 1) ->
+        find(str, 0, scan_len - 1, found)
+      {false, :too_short} ->
+        found
+      _ ->
+        find(str, current_pos + 1, scan_len, found)
+    end
+  end
+
+  defp scan_substr(str, begin, scan_len) do
+    str |> String.slice(begin..(begin + scan_len))
+  end
+
+  defp last_found(found) when is_list(found) and length(found) == 0, do: ""
+  defp last_found([head|_]), do: head
+
   # a palindrome is at least 3 characters
-  defp is_palindrome(str) when byte_size(str) < 3, do: :too_short
+  defp is_palindrome(str) when byte_size(str) < @min_palindrome_len do
+    {false, :too_short}
+  end
+
   defp is_palindrome(str) when is_bitstring(str) do
-    String.reverse(str) == str
+    {String.reverse(str) == str, :ok}
   end
 end
 
--- test/drom_test.exs
+++ test/drom_test.exs
@@ -3,50 +3,49 @@ defmodule DromTest do
   doctest Drom
 
   test "returns nil when the string is nil" do
-    str = nil
-    assert Drom.find_palindrome(str) == nil
+    assert Drom.find(nil) == nil
   end
 
   test "when the string is a palindrome" do
     str = "aca"
-    assert Drom.find_palindrome(str) == {:ok, str}
+    assert Drom.find(str) == [str]
 
     str = "ada"
-    assert Drom.find_palindrome(str) == {:ok, str}
+    assert Drom.find(str) == [str]
   end
 
   test "when the palindrome is in the beginning" do
     str = "acablah"
-    assert Drom.find_palindrome(str) == {:ok, "aca"}
+    assert Drom.find(str) == ["aca"]
   end
 
   test "when the palindrome is in the middle" do
     str = "supacayoo"
-    assert Drom.find_palindrome(str) == {:ok, "aca"}
+    assert Drom.find(str) == ["aca"]
   end
 
   test "when the palindrome is in the end" do
     str = "supyo11aca"
-    assert Drom.find_palindrome(str) == {:ok, "aca"}
+    assert Drom.find(str) == ["aca"]
   end
 
   test "when the palindrome is longer" do
     str = "acaoooioooblah"
-    assert Drom.find_palindrome(str) == {:ok, "oooiooo"}
+    assert Drom.find(str) == ["oooiooo"]
   end
 
   test "when there are no palindromes" do
     str = "123456"
-    assert Drom.find_palindrome(str) == :not_found
+    assert Drom.find(str) == []
   end
 
   test "when the long str is a palindrome" do
     str = "abdedba"
-    assert Drom.find_palindrome(str) == {:ok, str}
+    assert Drom.find(str) == [str]
   end
 
-  test "when there are more than one palindrome, find the first one" do
+  test "when there are more than one palindrome, find them all" do
     str = "abracadabra"
-    assert Drom.find_palindrome(str) == {:ok, "aca"}
+    assert Drom.find(str) == ["ada", "aca"]
   end
 end
