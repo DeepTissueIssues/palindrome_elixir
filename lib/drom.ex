2acf7815ea044c35a5e2040f476dd7829f6f3819
Feifan Wang <feifanw@gmail.com>
2017-01-13 00:40:07 -0500

    refactor: remove the `run` function

--- lib/drom.ex
+++ lib/drom.ex
@@ -1,67 +1,30 @@
 defmodule Drom do
-  def run(nil), do: nil
-  def run(str) when is_bitstring(str) do
-    cond do
-      is_palindrom(str) -> str
-      true ->
-        case find_palindrom(str) do
-          {:ok, palindrom} -> palindrom
-          {:not_found} -> :not_found
-        end
-    end
+  def find_palindrome(nil), do: nil
+  def find_palindrome(str) when is_bitstring(str) do
+    find_palindrome(str, 0, String.length(str) - 1)
   end
 
-  def find_palindrom(str) when is_bitstring(str) do
-    find_palindrom(str, 0, 2)
-  end
-
-  def find_palindrom(str, current_position, scan_len) do
+  def find_palindrome(str, current_position, scan_len) do
     end_position = current_position + scan_len
-    scan_range = current_position..end_position
-    scanned_substr = String.slice(str, scan_range)
+    scanned_substr = String.slice(str, current_position..end_position)
 
-    # IO.inspect("$$$$$$$$")
-    # IO.inspect("Str: #{str}")
-    # IO.inspect("Current pos: #{current_position}")
-    # IO.inspect("Scanned substr: #{scanned_substr}")
-    # IO.inspect("$$$$$$$$")
-
-    case is_palindrom(scanned_substr) do
+    case is_palindrome(scanned_substr) do
       true -> {:ok, scanned_substr}
+      :too_short when scan_len > 2 ->
+        find_palindrome(str, 0, scan_len - 1)
       :too_short -> :not_found
-      _ -> find_palindrom(str, current_position + 1, scan_len)
+      _ -> find_palindrome(str, current_position + 1, scan_len)
     end
   end
 
-  # a palindrom is at least 3 characters
-  defp is_palindrom(str) when byte_size(str) < 3, do: :too_short
-  defp is_palindrom(str) when is_bitstring(str) do
+  # a palindrome is at least 3 characters
+  defp is_palindrome(str) when byte_size(str) < 3, do: :too_short
+  defp is_palindrome(str) when is_bitstring(str) do
     String.reverse(str) == str
   end
 
-  defp is_palindrom(str) when is_list(str) do
-    Enum.join(str) |> is_palindrom
+  defp is_palindrome(str) when is_list(str) do
+    Enum.join(str) |> is_palindrome
   end
-
-  # def find_palindrom(str) when is_bitstring(str) do
-  #   str
-  #   |> String.graphemes
-  #   |> find_palindrom
-  # end
-
-  # def find_palindrom([head|tail]) do
-  #   find_palindrom(head, tail)
-  # end
-
-  # def find_palindrom(memo, []), do: :not_found
-  # def find_palindrom(memo, [head|tail]) do
-  #   IO.inspect("head #{memo}")
-  #   IO.inspect("tail #{tail}")
-
-  #   cond do
-  #     is_palindrom(memo) -> {:ok, memo}
-  #     is_palindrom(tail) -> {:ok, Enum.join(tail)}
-  #     true -> find_palindrom("#{memo}#{head}", tail)
-  #   end
-  # end
 end
+
--- test/drom_test.exs
+++ test/drom_test.exs
@@ -4,29 +4,44 @@ defmodule DromTest do
 
   test "returns nil when the string is nil" do
     str = nil
-    assert Drom.run(str) == nil
+    assert Drom.find_palindrome(str) == nil
   end
 
-  test "when the string is a palindrom" do
+  test "when the string is a palindrome" do
     str = "aca"
-    assert Drom.run(str) == str
+    assert Drom.find_palindrome(str) == {:ok, str}
 
     str = "ada"
-    assert Drom.run(str) == str
+    assert Drom.find_palindrome(str) == {:ok, str}
   end
 
-  test "when the palindrom is in the beginning" do
+  test "when the palindrome is in the beginning" do
     str = "acablah"
-    assert Drom.run(str) == "aca"
+    assert Drom.find_palindrome(str) == {:ok, "aca"}
   end
 
-  test "when the palindrom is in the middle" do
+  test "when the palindrome is in the middle" do
     str = "supacayoo"
-    assert Drom.run(str) == "aca"
+    assert Drom.find_palindrome(str) == {:ok, "aca"}
   end
 
-  test "when the palindrom is in the end" do
+  test "when the palindrome is in the end" do
     str = "supyo11aca"
-    assert Drom.run(str) == "aca"
+    assert Drom.find_palindrome(str) == {:ok, "aca"}
+  end
+
+  test "when the palindrome is longer" do
+    str = "acaoooioooblah"
+    assert Drom.find_palindrome(str) == {:ok, "oooiooo"}
+  end
+
+  test "when there are no palindromes" do
+    str = "123456"
+    assert Drom.find_palindrome(str) == :not_found
+  end
+
+  test "when the long str is a palindrome" do
+    str = "abdedba"
+    assert Drom.find_palindrome(str) == {:ok, str}
   end
 end
