4fa8052ffdea95d463bf5d08c008d8e47fd37e6a
Feifan Wang <feifanw@gmail.com>
2017-01-13 01:33:12 -0500

    fix: uncomment the tests...

--- test/drom_test.exs
+++ test/drom_test.exs
@@ -2,48 +2,48 @@ defmodule DromTest do
   use ExUnit.Case
   doctest Drom
 
-  # test "returns nil when the string is nil" do
-  #   str = nil
-  #   assert Drom.find_palindrome(str) == nil
-  # end
+  test "returns nil when the string is nil" do
+    str = nil
+    assert Drom.find_palindrome(str) == nil
+  end
 
-  # test "when the string is a palindrome" do
-  #   str = "aca"
-  #   assert Drom.find_palindrome(str) == {:ok, str}
+  test "when the string is a palindrome" do
+    str = "aca"
+    assert Drom.find_palindrome(str) == {:ok, str}
 
-  #   str = "ada"
-  #   assert Drom.find_palindrome(str) == {:ok, str}
-  # end
+    str = "ada"
+    assert Drom.find_palindrome(str) == {:ok, str}
+  end
 
-  # test "when the palindrome is in the beginning" do
-  #   str = "acablah"
-  #   assert Drom.find_palindrome(str) == {:ok, "aca"}
-  # end
+  test "when the palindrome is in the beginning" do
+    str = "acablah"
+    assert Drom.find_palindrome(str) == {:ok, "aca"}
+  end
 
-  # test "when the palindrome is in the middle" do
-  #   str = "supacayoo"
-  #   assert Drom.find_palindrome(str) == {:ok, "aca"}
-  # end
+  test "when the palindrome is in the middle" do
+    str = "supacayoo"
+    assert Drom.find_palindrome(str) == {:ok, "aca"}
+  end
 
-  # test "when the palindrome is in the end" do
-  #   str = "supyo11aca"
-  #   assert Drom.find_palindrome(str) == {:ok, "aca"}
-  # end
+  test "when the palindrome is in the end" do
+    str = "supyo11aca"
+    assert Drom.find_palindrome(str) == {:ok, "aca"}
+  end
 
-  # test "when the palindrome is longer" do
-  #   str = "acaoooioooblah"
-  #   assert Drom.find_palindrome(str) == {:ok, "oooiooo"}
-  # end
+  test "when the palindrome is longer" do
+    str = "acaoooioooblah"
+    assert Drom.find_palindrome(str) == {:ok, "oooiooo"}
+  end
 
-  # test "when there are no palindromes" do
-  #   str = "123456"
-  #   assert Drom.find_palindrome(str) == :not_found
-  # end
+  test "when there are no palindromes" do
+    str = "123456"
+    assert Drom.find_palindrome(str) == :not_found
+  end
 
-  # test "when the long str is a palindrome" do
-  #   str = "abdedba"
-  #   assert Drom.find_palindrome(str) == {:ok, str}
-  # end
+  test "when the long str is a palindrome" do
+    str = "abdedba"
+    assert Drom.find_palindrome(str) == {:ok, str}
+  end
 
   test "when there are more than one palindrome, find the first one" do
     str = "abracadabra"
