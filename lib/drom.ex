2602a12858454d8256a8446cc5cf5f577edbc211
Feifan Wang <feifanw@gmail.com>
2017-01-16 14:09:40 -0500

    rename private func

--- lib/drom.ex
+++ lib/drom.ex
@@ -3,37 +3,37 @@ defmodule Drom do
 
   def find(nil), do: nil
   def find(str) when is_bitstring(str) do
-    find(str, 0, String.length(str) - 1, [], 0)
+    _find(str, 0, byte_size(str) - 1, [], 0)
   end
 
   # no way there are more palindromes long enough
   # the next node down is useless, back out of this branch and start a new branch
-  defp find(_str, current_pos, scan_len, found, longest)
+  defp _find(_str, current_pos, scan_len, found, longest)
     when current_pos == 0
     and  scan_len + 1 < longest,
     do: found
 
-  defp find(str, current_pos, scan_len, found, longest)
+  defp _find(str, current_pos, scan_len, found, longest)
     when current_pos != 0
     and  (scan_len - current_pos + 1) < longest,
-    do: find(str, 0, scan_len - 1, found, longest)
+    do: _find(str, 0, scan_len - 1, found, longest)
 
-  defp find(str, current_pos, scan_len, found, longest) do
+  defp _find(str, current_pos, scan_len, found, longest) do
     substr = String.slice(str, current_pos..scan_len)
 
     case is_palindrome(substr) do
       {true, :ok} when byte_size(substr) < longest ->
         found
       {true, :ok} when byte_size(substr) > longest ->
-        find(str, 0, scan_len - 1, [substr], byte_size(substr))
+        _find(str, 0, scan_len - 1, [substr], byte_size(substr))
       {true, :ok} ->
-        find(str, 0, scan_len - 1, [substr|found], byte_size(substr))
+        _find(str, 0, scan_len - 1, [substr|found], byte_size(substr))
       {false, :too_short} when scan_len > (@min_palindrome_len - 1) ->
-        find(str, 0, scan_len - 1, found, longest)
+        _find(str, 0, scan_len - 1, found, longest)
       {false, :too_short} ->
         found
       {false, :ok} ->
-        find(str, current_pos + 1, scan_len, found, longest)
+        _find(str, current_pos + 1, scan_len, found, longest)
     end
   end
 
